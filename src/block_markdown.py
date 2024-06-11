d1dbc65d819fa25e086102f665af84caf27c1bb1
Tyler Gregory <tylergreg95@gmail.com>
2024-06-11 17:28:34 -0400

    implemented block_to_block_type() and tests

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,11 @@
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list = "ordered_list"
+
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split('\n\n')
     for i in range(len(blocks)):
@@ -8,3 +16,36 @@ def markdown_to_blocks(markdown):
             blocks_empties_removed.append(blocks[i])
             
     return blocks_empties_removed
+
+def block_to_block_type(block):
+    if block.startswith("#"):
+        return block_type_heading
+    if block.startswith("```") and block.endswith("```"):
+        return block_type_code
+    if block.startswith(">"):
+        lines = block.split("\n")
+        for line in lines:
+            if not line.startswith(">"):
+                return block_type_paragraph
+        return block_type_quote
+    if block.startswith("* "):
+        lines = block.split("\n")
+        for line in lines:
+            if not (line.startswith("* ")):
+                return block_type_paragraph
+        return block_type_unordered_list
+    if block.startswith("- "):
+        lines = block.split("\n")
+        for line in lines:
+            if not (line.startswith("- ")):
+                return block_type_paragraph
+        return block_type_unordered_list
+    if block.startswith("1. "):
+        lines = block.split("\n")
+        counter = 1
+        for line in lines:
+            if not line.startswith(f"{counter}. "):
+                return block_type_paragraph
+            counter += 1
+        return block_type_ordered_list
+    return block_type_paragraph
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -2,6 +2,13 @@ import unittest
 
 from block_markdown import (
     markdown_to_blocks,
+    block_to_block_type,
+    block_type_heading,
+    block_type_code,
+    block_type_ordered_list,
+    block_type_paragraph,
+    block_type_quote,
+    block_type_unordered_list,
 )
 
 class TestBlockMarkdown(unittest.TestCase):
@@ -31,3 +38,58 @@ This is the same paragraph on a new line
         expected = ["This is **bolded** paragraph", "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line", "* This is a list\n* with items"]
         blocks = markdown_to_blocks(markdown)
         self.assertListEqual(expected, blocks)
+    
+    def test_block_to_block_type_heading(self):
+        markdown = "# This is a heading"
+        blocks = markdown_to_blocks(markdown)
+        self.assertEqual(block_to_block_type(blocks[0]), block_type_heading)
+
+    def test_block_to_block_type_code(self):
+        markdown = "``` This is some code that does things\nand some more things```"
+        blocks = markdown_to_blocks(markdown)
+        self.assertEqual(block_to_block_type(blocks[0]), block_type_code)
+
+    def test_block_to_block_type_quote(self):
+        markdown = "> This is a quote\n> And here is the second part\n>And here is the last part"
+        blocks = markdown_to_blocks(markdown)
+        self.assertEqual(block_to_block_type(blocks[0]), block_type_quote)
+    
+    def test_block_to_block_type_not_quote(self):
+        markdown = "> This is a quote\n> And here is the second part\nAnd here is the last part"
+        blocks = markdown_to_blocks(markdown)
+        self.assertEqual(block_to_block_type(blocks[0]), block_type_paragraph)
+
+    def test_block_to_block_type_unordered_list(self):
+        markdown = "* This is an item in an unordered list\n* And here is the second item\n* And here is the last item"
+        blocks = markdown_to_blocks(markdown)
+        self.assertEqual(block_to_block_type(blocks[0]), block_type_unordered_list)
+
+    def test_block_to_block_type_unordered_list_hyphen(self):
+        markdown = "- This is an item in an unordered list\n- And here is the second item\n- And here is the last item"
+        blocks = markdown_to_blocks(markdown)
+        self.assertEqual(block_to_block_type(blocks[0]), block_type_unordered_list)
+
+    def test_block_to_block_type_not_unordered_list(self):
+        markdown = "* This is an item in an unordered list\n And here is the second item\n* And here is the last item"
+        blocks = markdown_to_blocks(markdown)
+        self.assertEqual(block_to_block_type(blocks[0]), block_type_paragraph)
+
+    def test_block_to_block_type_ordered_list(self):
+        markdown = "1. This is an item in an unordered list\n2. And here is the second item\n3. And here is the last item"
+        blocks = markdown_to_blocks(markdown)
+        self.assertEqual(block_to_block_type(blocks[0]), block_type_ordered_list)
+    
+    def test_block_to_block_type_skip_number_ordered_list(self):
+        markdown = "1. This is an item in an unordered list\n3. And here is the second item\n4. And here is the last item"
+        blocks = markdown_to_blocks(markdown)
+        self.assertEqual(block_to_block_type(blocks[0]), block_type_paragraph)
+    
+    def test_block_to_block_type_missing_number_ordered_list(self):
+        markdown = "1. This is an item in an unordered list\n. And here is the second item\n3. And here is the last item"
+        blocks = markdown_to_blocks(markdown)
+        self.assertEqual(block_to_block_type(blocks[0]), block_type_paragraph)
+    
+    def test_block_to_block_type_misordered_number_ordered_list(self):
+        markdown = "1. This is an item in an unordered list\n3. And here is the second item\n2. And here is the last item"
+        blocks = markdown_to_blocks(markdown)
+        self.assertEqual(block_to_block_type(blocks[0]), block_type_paragraph)
